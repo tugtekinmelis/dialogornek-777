4a97aa3924f23f8aa300c132f9c448d0eb917a1f
tugtekinmelis <145451246+tugtekinmelis@users.noreply.github.com>
2024-02-13 10:18:29 +0300

    Add files via upload

---
+++ dialog ornek-777/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace dialog_ornek_777
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnSec = new System.Windows.Forms.Button();
+            this.btnRenk = new System.Windows.Forms.Button();
+            this.pbResimSec = new System.Windows.Forms.PictureBox();
+            this.rtbMesaj = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResimSec)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnSec
+            // 
+            this.btnSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSec.Location = new System.Drawing.Point(45, 44);
+            this.btnSec.Name = "btnSec";
+            this.btnSec.Size = new System.Drawing.Size(124, 58);
+            this.btnSec.TabIndex = 0;
+            this.btnSec.Text = "Resim Seç";
+            this.btnSec.UseVisualStyleBackColor = true;
+            this.btnSec.Click += new System.EventHandler(this.btnSec_Click);
+            // 
+            // btnRenk
+            // 
+            this.btnRenk.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnRenk.Location = new System.Drawing.Point(433, 44);
+            this.btnRenk.Name = "btnRenk";
+            this.btnRenk.Size = new System.Drawing.Size(124, 58);
+            this.btnRenk.TabIndex = 1;
+            this.btnRenk.Text = "Renk Değiştir";
+            this.btnRenk.UseVisualStyleBackColor = true;
+            this.btnRenk.Click += new System.EventHandler(this.btnRenk_Click);
+            // 
+            // pbResimSec
+            // 
+            this.pbResimSec.Location = new System.Drawing.Point(45, 133);
+            this.pbResimSec.Name = "pbResimSec";
+            this.pbResimSec.Size = new System.Drawing.Size(298, 305);
+            this.pbResimSec.TabIndex = 2;
+            this.pbResimSec.TabStop = false;
+            // 
+            // rtbMesaj
+            // 
+            this.rtbMesaj.Location = new System.Drawing.Point(407, 133);
+            this.rtbMesaj.Name = "rtbMesaj";
+            this.rtbMesaj.Size = new System.Drawing.Size(355, 305);
+            this.rtbMesaj.TabIndex = 3;
+            this.rtbMesaj.Text = "Melisa\nKübra\nHayrunnisa\nRabia\nSukeyna\nYasemin\nİrem\nSude\nElifsu\nCeren";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtbMesaj);
+            this.Controls.Add(this.pbResimSec);
+            this.Controls.Add(this.btnRenk);
+            this.Controls.Add(this.btnSec);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbResimSec)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnSec;
+        private System.Windows.Forms.Button btnRenk;
+        private System.Windows.Forms.PictureBox pbResimSec;
+        private System.Windows.Forms.RichTextBox rtbMesaj;
+    }
+}
+
---
+++ dialog ornek-777/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dialog_ornek_777
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSec_Click(object sender, EventArgs e)
+        {
+            DialogResult sonuc = openFileDialog1.ShowDialog();
+
+            if (sonuc == DialogResult.OK)
+            {
+                pbResimSec.ImageLocation = openFileDialog1.FileName;
+            }
+        }
+
+        private void btnRenk_Click(object sender, EventArgs e)
+        {
+            DialogResult sonuc = colorDialog1.ShowDialog();
+
+            if(sonuc == DialogResult.OK)
+            {
+                rtbMesaj.SelectionColor = colorDialog1.Color;
+            }
+        }
+    }
+}
